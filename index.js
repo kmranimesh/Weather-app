2fe13dcebd2df2a4bf8cb808af580efbbd1c5d0b
Animesh Kumar <83393501+kmranimesh@users.noreply.github.com>
2024-06-08 01:02:21 +0530

    API fetch succesful

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 console.log('Hello, Animesh');
+
+const API_KEY = "b25832a5e0ff3f0b092cf057be0ac669";
+
+
+
+async function showWeather() {
+    // let latitude = 15.3333;
+    // let longitude = 74.0833;
+    let city = "ranchi";
+
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}`);
+
+    const data = await response.json();
+    console.log("Weather data:-> " + data);
+
+    let newPara = document.createElement('p');
+    let tempInCelsius = data.main.temp - 273.15;
+    newPara.textContent = `${tempInCelsius.toFixed(2)} °C`;
+
+
+    document.body.appendChild(newPara);
+}
